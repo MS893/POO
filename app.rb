8102efc9c4887610bc09050207b9348a2a1978f7
MS893 <ms893@free.fr>
2025-09-22 20:52:52 +0200

    new

--- app.rb
+++ app.rb
@@ -6,13 +6,19 @@ Bundler.require
 # lignes qui appellent les fichiers lib/user.rb et lib/event.rb
 require_relative 'lib/user'
 require_relative 'lib/event'
-
+require_relative 'lib/workevent'
 # binding.pry
 User.new("julie@julie.com", 35)
 User.new("jean@jean.com", 23)
 User.new("claude@claude.com", 75)
 user_1 = User.find_by_email("claude@claude.com")
 puts "Voici l'age du User trouvé : #{user_1.age}"
+puts
+meeting = WorkEvent.new("2019-01-17 09:00", 70, "réunion de travail", ["julie@julie.com", "jean@jean.com"]) # je crée un WorkEvent
+puts meeting.is_past? # on obtient bien true
+puts meeting.to_s # on obtient l'affichage de l'objet avec les identifiants bruts des User
+meeting.postpone_24h
+puts meeting.start_date # on confirme qu'elle a été déplacée au 2019-01-18, 9h
 puts
 
 my_event = Event.new(Time.now.strftime("%Y-%m-%d %H:%M"), 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
